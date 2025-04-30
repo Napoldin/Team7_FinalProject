9c237233928a5e92c97758738a31f9ab8e11967c
caseyc141 <casey21222@gmail.com>
2025-04-30 10:27:04 -0400

    Added helpfule help message

--- Bot/Commands/help.js
+++ Bot/Commands/help.js
@@ -1,6 +1,6 @@
 //help.js
 
 module.exports.Help = async (message) => {
-    return message.reply("Test Help Statement");
+    return message.reply("To use the commands, Tango must be in a voice channel using the !join command. This adds Tango to the voice chat the user is currently in. Then, use either the !tts or !transcribe command to use their respective functions. Then once done, use the !leave to remove the bot from its current channel. For further explanation of commands, please read 'About Tango on our website.'");
 }
   
